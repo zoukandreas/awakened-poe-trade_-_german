5d80c1de2b7f98c28b35a2f99a82aa92ded671e8
Zouk Andreas <andy.79@gmx.net>
2024-02-23 15:56:25 +0100

    integrated german language into the app

--- renderer/src/web/client-log/client-log.ts
+++ renderer/src/web/client-log/client-log.ts
@@ -139,5 +139,6 @@ const TRADE_BULK_WHISPER = {
   'en': /^Hi, I'd like to buy your (?<item>.+) for my (?<price>.+) in (?<league>.+)\.(?<message>.+)?$/,
   'ru': /^Здравствуйте, хочу купить у вас (?<item>.+) за (?<price>.+) в лиге (?<league>.+)\.(?<message>.+)?$/,
   'cmn-Hant': /^你好，我想用 (?<price>.+) 購買 (?<item>.+) in (?<league>.+)\.(?<message>.+)?$/,
-  'ko': /^_FIX_ME_$/
+  'ko': /^_FIX_ME_$/,
+  'de': /^Hi, ich möchte dein (?<item>.+) für mein (?<price>.+) in der (?<league>.+)-Liga kaufen\.(?<message>.+)?$/
 }
